6f12977bb506feb8a77a3a8a21c5583cf8479a66
Dennis <cjun1775@gmail.com>
2025-05-10 13:45:34 +1000

    npm run server error fix

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -163,7 +163,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
